43e0cd70d405b342126d85694ee0d62438d22e1f
mzakiDev-eng <muhamadzakidev@gmail.com>
2026-07-31 15:38:30 -0700

    MongoDB CRUD

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 //Routes
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.get('/' ,(req, res)=>{
-
+  res.render('home')
 });
 app.get('/show-contact' ,(req, res)=>{
-
+  res.render('show-contact')
 });
 app.get('/add-contact' ,(req, res)=>{
-
+ res.render('add-contact')
 });
 app.post('/add-contact' ,(req, res)=>{
 
 });
 app.get('/update-contact' ,(req, res)=>{
-
+ res.render('update-contact')
 });
 app.post('/update-contact' ,(req,res)=>{
 
 });
 app.get('/delete-contact' ,(req,res)=>{
-    
+
 })
 
 
