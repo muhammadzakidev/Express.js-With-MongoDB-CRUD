5d052ccac08fb16fbf1b1085249cb6ce3cc35bbf
mzakiDev-eng <muhamadzakidev@gmail.com>
2026-08-03 21:07:49 -0700

    refactor: migrate project from CommonJS to ES modules

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,35 +1,90 @@
-import express from 'express'
-import Contact from '../model/contactModel.js'
+import express from "express";
+import Contact from "../model/contactModel.js";
+import mongoose from "mongoose";
 
-const homeContact = async(req, res)=>{
-  const contacts = await Contact.find();
-  res.render('home' , {contacts});
-}
+const homeContact = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.render("home", { contacts });
+  } catch (error) {
+    res.render("505", { message: error });
+  }
+};
 
-const showContact = async (req, res)=>{
-  const contact = await Contact.findById(req.params.id); 
-  res.render('show-contact' ,{contact})
-}
-const addContact = async(req, res)=>{
- res.render('add-contact');
-}
-const addPostContact = async (req, res)=>{
+const showContact = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.render("404", { message: "Invalid-Id" });
+    }
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.render("405", { message: "could not show content" });
+    }
+    res.render('show-contact' , {contact});
+  } catch (error) {
+         res.render('505' ,{message: error});
+  }
+};
+const addContact = async (req, res) => {
+  res.render("add-contact");
+};
+const addPostContact = async (req, res) => {
   const contact = await Contact.create(req.body);
   res.redirect("/");
-  
-}
-const updateContact = async (req, res)=>{
+};
+const updateContact = async (req, res) => {
+  try {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id));
+    {
+      res.render('404' , {message: "Invalid-id"});      
+    }
+
   const contact = await Contact.findById(req.params.id);
- res.render('update-contact' , {contact});
-}
-const updateContactById = async(req,res)=>{
-await Contact.findByIdAndUpdate(req,params.id , req.body);
-res.redirect('/');
-}
+  if(!contact)
+  {
+     return res.render('404' , {message: "Not update yet"});
+  }
+  res.render("update-contact", { contact });
+    
+  } catch (error) {
+    res.render('505' , {message: error});
+  }
+};
+const updateContactById = async (req, res) => {
+    try {
+      if(!mongoose.Types.ObjectId.isValid(req.params.id))
+      {
+        return res.render('404' , {message: "could not update yet or Invalid Id"});
+      }
+      const contact = await Contact.findByIdAndUpdate(req.params.id , req.body);
+      if(!contact)
+      {
+        return res.render('404' , {message: 'Invalid-Id'})
+      }
+      res.redirect('/'); 
+    } catch (error) {
+        res.render('505' , {message: error})
+    }
+};
 
-const deleteContact = async(req,res)=>{
-   await Contact.findByIdAndDelete(req.params.id);
+const deleteContact = async (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id))
+  {
+     return res.render('404' , {message: "invalid-id"});
+  }
+  const contact = await Contact.findByIdAndDelete(req.params.id)
+  if(!contact)
+  {
+     return res.render('404' , {message: 'could not delete '});
+  }
   res.redirect('/');
-
-}
-export default {homeContact , showContact , addContact, addPostContact, updateContact, updateContactById, deleteContact} ;
+};
+export default {
+  homeContact,
+  showContact,
+  addContact,
+  addPostContact,
+  updateContact,
+  updateContactById,
+  deleteContact,
+};
